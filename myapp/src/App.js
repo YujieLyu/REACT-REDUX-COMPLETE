fa4f038a1f6e955d5ba5f19d81fd3b4349058102
YujieLyu <jercylv@gmail.com>
2020-02-13 17:30:15 +1100

    Form stuff

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Jessie from './Jessie';
+import AddJess from './AddJess';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>My first React app!</h1>
-     <Jessie name="Yujie" age="40" belt="black"/>
-     <Jessie name="Zoe" age="2" belt="white"/>
-    </div>
-  );
+class App extends Component {
+  state={
+    jessie:[
+      {name:'Amy', age:'14', belt:'white', id:1},
+      {name:'Bella', age:'24', belt:'yellow', id:2},
+      {name:'Cindy', age:'35', belt:'black', id:3}
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+       <h1>My first React app!</h1>
+       <Jessie jessie={this.state.jessie}/>
+       <AddJess />
+      </div>
+    );
+  }
 }
 
 export default App;
--- myapp/src/Jessie.js
+++ myapp/src/Jessie.js
@@ -1,19 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+// Why const here? Why not function?
+const Jessie=({jessie})=>{
 
-class Jessie extends Component{
-    render(){
         // console.log(this.props);
         // Use variables is for reuse
-        const {name,age,belt}=this.props;
+        // TODO: Need help here
+        //Method#1:
+        // const jessList=jessie.map(jess=>{
+        //     if(jess.age>20){
+        //         return(
+        //             <div className="jessie" key={jess.id}>
+        //             <div>Name: {jess.name}</div>
+        //             <div>Age: {jess.age}</div>
+        //             <div>Belt: {jess.belt}</div>
+        //             </div>
+        //         )
+        //     }else{
+        //         return null
+        //     }
+        //     // jess here is the individual element inside the list, it returns its own jsx output
+            
+        // })
+        //Method#2:
+        const jessList=jessie.map(jess=>{
+            return jess.age>20 ? (
+                <div className="jessie" key={jess.id}>
+                <div>Name: {jess.name}</div>
+                <div>Age: {jess.age}</div>
+                <div>Belt: {jess.belt}</div>
+                </div> 
+            ):null
+        })
         return(
-            <div className="jessie">
-                <div>Name: {name}</div>
-                <div>Age: {age}</div>
-                <div>Belt: {belt}</div>
-
+            <div className="jess-list">
+                {jessList}
             </div>
         )
     }
-}
 
 export default Jessie
