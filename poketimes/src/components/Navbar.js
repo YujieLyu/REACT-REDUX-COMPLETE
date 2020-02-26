94f6a151a92424c314ecae21ec653c051bef1044
YujieLyu <jercylv@gmail.com>
2020-02-26 16:59:26 +1100

    New feature - Fetch and display data Using a lifecycle hock component

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {Link,NavLink, withRouter} from 'react-router-dom'
 
 const Navbar=(props)=>{
-    setTimeout(()=>{
-        props.history.push('/about')
-    },2000);
+    // setTimeout(()=>{
+    //     props.history.push('/about')
+    // },2000);
     return(
         <nav className='nav-wrapper red darken-3'>
             <div className="container">
@@ -20,4 +20,5 @@ const Navbar=(props)=>{
     )
 };
 
-export default withRouter(Navbar)
+//withRouter make Navbar as the supercharged components
+export default Navbar
