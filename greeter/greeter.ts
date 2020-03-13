84455a2b411d883488789dd29634f9457cea48ad
YujieLyu <jercylv@gmail.com>
2020-03-13 23:24:30 +1100

    Random check

--- greeter/greeter.ts
+++ greeter/greeter.ts
@@ -5,16 +5,18 @@ class Student {
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
 }
+
 //Didn't see any relations between Student and Person,
 //how could we put the user(instance of Student) as a argument of greeter(person:Person)?
 interface Person {
     firstName: string;
     lastName: string;
+
 }
 
 function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-let user =new Student("Jane","M","May");
+let user = new Student("Jane", "M", "Lee");
 document.body.textContent = greeter(user);
