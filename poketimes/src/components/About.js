e0038a6765df10097ef294ccb655117ca78e6fda
YujieLyu <jessie.yujie.lyu@gmail.com>
2020-06-26 23:58:45 +1000

    poketimes with react-router

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About=()=>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>
+            In Chapter Two, get introduced to React components. They are the building blocks of any React Application you will build. Go ahead and review that chapter if you are not yet comfortable with React components and then head back here.
+            </p>
+        </div>
+    )
+}
+
+export default About;
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home=()=>{
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>In Chapter One, get introduced to React with ES6. If you are new to React , simply need a refresher, or need a gentle introduction to the ES6 features that are most frequently used throughout this book, review chapter one.</p>
+        </div>
+    )
+}
+
+export default Home;
