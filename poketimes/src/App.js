fede24e469fd511ed5e7fae5a6e8f41c5babff70
YujieLyu <jessie.yujie.lyu@gmail.com>
2020-06-27 14:47:26 +1000

    axios, hoc, programming redirect, switch...

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Post from './components/Post'
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route path='/' component={Home} />
-          <Route path='/about' component={About}/>
-          <Route path='/contact' component={Contact}/>
-          
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png'
 
-const Home=()=>{
-    return(
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>In Chapter One, get introduced to React with ES6. If you are new to React , simply need a refresher, or need a gentle introduction to the ES6 features that are most frequently used throughout this book, review chapter one.</p>
-        </div>
-    )
+class Home extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                // console.log(res)
+                this.setState({
+                    posts: res.data.slice(0, 10)
+                })
+            })
+    }
+
+    render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <img src={Pokeball}  alt="a pokeball"/>
+                        <div className="card-content">
+                            <Link to={'/' + post.id}>
+                                <span className="card-title text-red">{post.title}</span>
+                            </Link>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>)
+            })
+        ) : (
+                <div className="center">No posts yet</div>
+            )
+        return (
+            <div className="container home">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
 }
 
 export default Home;
