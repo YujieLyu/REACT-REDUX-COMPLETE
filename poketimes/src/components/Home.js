874c796490c99ae3f2fcb71d3594528ecb3f53b3
YujieLyu <jessie.yujie.lyu@gmail.com>
2020-06-27 14:51:09 +1000

    format code

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import Pokeball from '../pokeball.png'
+import Pokeball from '../pokeball.png';
+import '../index.css'
 
 class Home extends Component {
 
